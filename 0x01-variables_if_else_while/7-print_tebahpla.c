595db351337afd1d5859425e4d4cb4fd5b872fb3
Sophiebelles <sofiyyaholagunju@gmail.com>
2022-10-16 18:52:29 -0700

    ohhh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in reverse
+ * Return: 0
+ */
+ int main(void)
+ {
+        char y;
+        for (y = 'z'; y >= 'a'; y--)
+		 putchar(y);
+
+	 putchar('\n');
+
+	 return (0);
+ }
