dce942b34d6ef626cf8f9548f0bcbb1d29229469
Sophiebelles <sofiyyaholagunju@gmail.com>
2022-10-16 18:10:26 -0700

    ion no

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints in lower and upper ccase
+ * Return: 0
+ */
+int main(void)
+{    
+	char u;
+        
+	for (u == 'a'; u <= 'z'; u++);
+	putchar (u);
+
+       for (u = 'A'; u <= 'Z'; u++);
+        putchar ('\n');
+
+        return(0);
+}
