35a31e7273c8b4f5776f6d90dfb32c327c9f3b63
Sophiebelles <sofiyyaholagunju@gmail.com>
2022-10-17 21:54:56 -0700

    4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,14 +4,16 @@
  * Return: 0
  */
 int main(void)
-{    
+{
 	char u;
-        
-	for (u == 'a'; u <= 'z'; u++);
-	putchar (u);
 
-       for (u = 'A'; u <= 'Z'; u++);
-        putchar ('\n');
+	for (u = 'a'; u <= 'z'; u++)
+		putchar (u);
 
-        return(0);
+	for (u = 'A'; u <= 'Z'; u++)
+		putchar(u);
+
+	putchar('\n');
+
+	return (0);
 }
