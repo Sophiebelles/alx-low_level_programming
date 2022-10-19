c5f3c645d3fc223c47bc86b8e6652ad6f5193940
Sophiebelles <sofiyyaholagunju@gmail.com>
2022-10-18 20:56:44 -0700

    is a task indeed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet -prints the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+
+	char y;
+
+	for (y = 'a'; y <= 'z'; y++)
+		_putchar(y);
+
+	_putchar('\n');
+}
+
