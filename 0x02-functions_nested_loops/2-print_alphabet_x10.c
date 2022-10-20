2dbbf47c5964b2cbd056917d34701c0f6fed87b3
Sophiebelles <sofiyyaholagunju@gmail.com>
2022-10-19 21:23:22 -0700

    plz help

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print 10 times a alpha in lwercase
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char lower;
+
+	while (count++ <= 9)
+	for lower = 'a'; lower <= 'z'; lower++
+		- putchar(lower);
+	-putchar('\n');
