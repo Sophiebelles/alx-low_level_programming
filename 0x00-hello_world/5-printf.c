fb6da1be22ef372b722a13b737f9abbfde18339a
Sophiebelles <sofiyyaholagunju@gmail.com>
2022-10-23 01:30:06 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - using printf,print a line of code
- * return:0
+ * return: 0
  */
 int main(void)
 {
 	printf("with proper grammar,but the outcome is a piece of art.\n");
 	return (0);
+}
