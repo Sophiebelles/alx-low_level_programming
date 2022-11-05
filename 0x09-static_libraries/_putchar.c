3645b314e9549012502a6275ea22e87f0a21a94a
Sophiebelles <sofiyyaholagunju@gmail.com>
2022-11-04 19:08:26 -0700

    static library hard

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to be printed
+ * Return: 1 on success
+ * On error,return -1, and error is set appropriately
+ */
+ int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
